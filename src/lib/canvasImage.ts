d5653e65a744b987c3471798b470e04adc3c8aa4
Pewww <jung22337@naver.com>
2020-05-23 20:16:26 +0900

    :zap: getImageData가 promise를 반환하도록 수정

--- src/lib/canvasImage.ts
+++ src/lib/canvasImage.ts
@@ -6,31 +6,85 @@ interface IGetImageDataParams {
 }
 
 interface ICanvasImage {
-  getImageData: (params: IGetImageDataParams) => ImageData;
+  filterBackgroundImg: (backgroundImg: string) => string;
+  loadImage: (src: string) => Promise<HTMLImageElement>;
+  getImageData: (params: IGetImageDataParams) => Promise<ImageData>;
+  drawImage: () => Promise<CanvasImageSource>;
+  isImgTag: (element: HTMLElement) => boolean;
 }
 
 export default class CanvasImage implements ICanvasImage {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
-  private imageWidth: number | SVGAnimatedLength;
-  private imageHeight: number | SVGAnimatedLength;
+  private image: CanvasImageSource;
 
   constructor(image: CanvasImageSource) {
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
 
-    this.imageWidth = image.width;
-    this.imageHeight = image.height;
-
-    this.ctx.drawImage(image, 0, 0);
+    this.image = image;
   }
 
-  getImageData({
+  filterBackgroundImg(backgroundImg: string) {
+    const [, url = ''] = backgroundImg.split('"');
+    return url;
+  }
+
+  loadImage(src: string): Promise<HTMLImageElement> {
+    return new Promise(resolve => {
+      const image = new Image();
+      image.setAttribute('src', src);
+      
+      image.onload = () => {
+        this.ctx.drawImage(image, 0, 0);
+        resolve(image);
+      };
+    });
+  }
+
+  isImgTag(element: HTMLElement) {
+    return element.tagName === 'IMG';
+  }
+
+  drawImage(): Promise<CanvasImageSource> {
+    try {
+      if (this.isImgTag(this.image as HTMLElement)) {
+        return new Promise(resolve => {
+          this.ctx.drawImage(this.image, 0, 0);
+          resolve(this.image);
+        });
+      }
+  
+      const cssObj = window.getComputedStyle(this.image as Element);
+      const backgroundImg = cssObj.getPropertyValue('background-image');
+  
+      return this.loadImage(
+        this.filterBackgroundImg(backgroundImg)
+      );
+    } catch(e) {
+      throw Error(e);
+    }
+  }
+
+  async getImageData({
     x = 0,
     y = 0,
-    width = this.imageWidth as number,
-    height = this.imageHeight as number
+    width: propsWidth = 0,
+    height: propsHeight = 0
   } = {}) {
-    return this.ctx.getImageData(x, y, width, height);
+    const {
+      width: _width,
+      height: _height
+    } = await this.drawImage();
+
+    const width = (propsWidth !== 0 && propsWidth < _width)
+      ? propsWidth
+      : _width;
+
+    const height = (propsHeight !== 0 && propsHeight < _height)
+      ? propsHeight
+      : _height;
+
+    return this.ctx.getImageData(x, y, width as number, height as number);
   }
 }
