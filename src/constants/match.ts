824fa5f9ef516854d6c175ff761260ad2205bf1c
Pewww <jung22337@naver.com>
2020-05-24 20:12:02 +0900

    :poop: 더 이상 사용되지 않는 src 폴더 제거

--- src/constants/match.ts
+++
@@ -1,7 +0,0 @@
-interface IMatchRegex {
-  MATCH_RGB_RGBA_COLOR: RegExp;
-}
-
-export const MATCH_REGEX: IMatchRegex = {
-  MATCH_RGB_RGBA_COLOR: /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/i
-};
