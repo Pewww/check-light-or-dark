7db1006659fbdc81f166cf872422073e2d292700
Pewww <jung22337@naver.com>
2020-06-08 11:53:14 +0900

    :arrow_up: utils 내 index.ts에서 모듈을 바로 export 하도록 구문 수정 및 패키지 버전 업데이트

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,28 +1,8 @@
 // @constants
-import {
-  R_DEGREE,
-  G_DEGREE,
-  B_DEGREE,
-  BRIGHTNESS_DEGREE
-} from './src/constants/degrees';
-import {MATCH_REGEX} from './src/constants/match';
-import {VALIDATE_REGEX} from './src/constants/validates';
+export * from './src/constants/degrees';
+export * from './src/constants/match';
+export * from './src/constants/validates';
 
 // @lib
-import getBrightness from './src/lib/brightness';
-import {
-  hexToRgb,
-  rgbaToRgb
-} from './src/lib/conversion';
-
-export {
-  R_DEGREE,
-  G_DEGREE,
-  B_DEGREE,
-  BRIGHTNESS_DEGREE,
-  MATCH_REGEX,
-  VALIDATE_REGEX,
-  getBrightness,
-  hexToRgb,
-  rgbaToRgb
-};
+export {default as getBrightness} from './src/lib/brightness';
+export * from './src/lib/conversion';
